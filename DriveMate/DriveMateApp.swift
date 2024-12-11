944107e372ca545869ed1e40f75655d87f50b7f8
Azmal Awasaf <aawsaf@mymedicalhub.com>
2024-12-11 15:23:23 +0600

    Initial Commit

---
+++ DriveMate/DriveMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveMateApp.swift
+//  DriveMate
+//
+//  Created by MMH on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DriveMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
